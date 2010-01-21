f941eed74266206f47dee3b7b2680ba9da803379
EirikJ <info@example.com>
2010-01-21 10:20:08 +0000

    Added ezinfo class
    git-svn-id: http://svn.projects.ez.no/nmcheckout@2 8b7787b4-0904-46e0-9f15-60bb4472c4eb

---
+++ trunk/ezinfo.php
@@ -0,0 +1,17 @@
+<?php
+
+class nmCheckoutInfo
+{
+    static function info()
+    {
+        return array(
+            'Name' => '<a href="http://projects.ez.no/nmcheckout">NM Checkout</a> extension',
+            'Version' => '1.0-0',
+            'Author' => "<a href='http://www.netmaking.no/'>Netmaking - Webdesign and CMS</a>",
+            'Copyright' => "Copyright (C) 2009-2010 Netmaking AS",
+            'License' => 'GNU General Public License v2.0',
+        );
+    }
+}
+
+?>
